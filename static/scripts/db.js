4aedda266cf32897bb998f4ee3397c034021bdba
Raknarrok <raknarrok@hotmail.com>
2023-06-23 12:50:35 -0600

    Checkout Feature Added

--- static/scripts/db.js
+++ static/scripts/db.js
@@ -1,110 +1,110 @@
 [
-    {
-      "id": 1,
-      "name": "Collar de perlas",
-      "quantity": 0,
-      "desc": "Collar de perlas de agua dulce, con cierre de plata 925",
-      "price": 1200,
-      "img": "../static/images/productos/collares/perlas.png",
-      "category": "collares"
-    },
-    {
-      "id": 2,
-      "name": "Collar de plata",
-      "quantity": 20,
-      "desc": "Collar de plata 925, con dije de perla de agua dulce",
-      "price": 1500,
-      "img": "../static/images/productos/collares/plata.png",
-      "category": "collares"
-    },
-    {
-      "id": 3,
-      "name": "Collar de oro",
-      "quantity": 30,
-      "desc": "Collar de oro 18k, con dije de perla de agua dulce",
-      "price": 2000,
-      "img": "../static/images/productos/collares/oro.png",
-      "category": "collares"
-    },
-    {
-      "id": 4,
-      "name": "Collar de diamantes",
-      "quantity": 20,
-      "desc": "Collar de oro 18k, con dije de perla de agua dulce y diamantes",
-      "price": 3000,
-      "img": "../static/images/productos/collares/diamantes.png",
-      "category": "collares"
-    },
-    {
-      "id": 5,
-      "name": "Pulsera de perlas",
-      "quantity": 20,
-      "desc": "Pulsera de perlas de agua dulce, con cierre de plata 925",
-      "price": 800,
-      "img": "../static/images/productos/pulseras/perlas.png",
-      "category": "pulseras"
-    },
-    {
-      "id": 6,
-      "name": "Pulsera de plata",
-      "quantity": 10,
-      "desc": "Pulsera de plata 925, con dije de perla de agua dulce",
-      "price": 1000,
-      "img": "../static/images/productos/pulseras/plata.png",
-      "category": "pulseras"
-    },
-    {
-      "id": 7,
-      "name": "Pulsera de oro",
-      "quantity": 10,
-      "desc": "Pulsera de oro 18k, con dije de perla de agua dulce",
-      "price": 1500,
-      "img": "../static/images/productos/pulseras/oro.png",
-      "category": "pulseras"
-    },
-    {
-      "id": 8,
-      "name": "Pulsera de diamantes",
-      "quantity": 15,
-      "desc": "Pulsera de oro 18k, con dije de perla de agua dulce y diamantes",
-      "price": 2500,
-      "img": "../static/images/productos/pulseras/diamantes.png",
-      "category": "pulseras"
-    },
-    {
-      "id": 9,
-      "name": "Anillo de perlas",
-      "quantity": 5,
-      "desc": "Anillo de perlas de agua dulce, con cierre de plata 925",
-      "price": 1000,
-      "img": "../static/images/productos/anillos/perlas.png",
-      "category": "anillos"
-    },
-    {
-      "id": 10,
-      "name": "Anillo de plata",
-      "quantity": 6,
-      "desc": "Anillo de plata 925, con dije de perla de agua dulce",
-      "price": 1200,
-      "img": "../static/images/productos/anillos/plata.png",
-      "category": "anillos"
-    },
-    {
-      "id": 11,
-      "name": "Anillo de oro",
-      "quantity": 3,
-      "desc": "Anillo de oro 18k, con dije de perla de agua dulce",
-      "price": 1800,
-      "img": "../static/images/productos/anillos/oro.png",
-      "category": "anillos"
-    },
-    {
-      "id": 12,
-      "name": "Anillo de diamantes",
-      "quantity": 2,
-      "desc": "Anillo de oro 18k, con dije de perla de agua dulce y diamantes",
-      "price": 3000,
-      "img": "../static/images/productos/anillos/diamantes.png",
-      "category": "anillos"
-    }
-  ]
+  {
+    "id": 1,
+    "name": "Collar de perlas",
+    "quantity": 0,
+    "desc": "Collar de perlas de agua dulce, con cierre de plata 925",
+    "price": 1200,
+    "img": "../static/images/productos/collares/perlas.png",
+    "category": "collares"
+  },
+  {
+    "id": 2,
+    "name": "Collar de plata",
+    "quantity": 20,
+    "desc": "Collar de plata 925, con dije de perla de agua dulce",
+    "price": 1500,
+    "img": "../static/images/productos/collares/plata.png",
+    "category": "collares"
+  },
+  {
+    "id": 3,
+    "name": "Collar de oro",
+    "quantity": 30,
+    "desc": "Collar de oro 18k, con dije de perla de agua dulce",
+    "price": 2000,
+    "img": "../static/images/productos/collares/oro.png",
+    "category": "collares"
+  },
+  {
+    "id": 4,
+    "name": "Collar de diamantes",
+    "quantity": 20,
+    "desc": "Collar de oro 18k, con dije de perla de agua dulce y diamantes",
+    "price": 3000,
+    "img": "../static/images/productos/collares/diamantes.png",
+    "category": "collares"
+  },
+  {
+    "id": 5,
+    "name": "Pulsera de perlas",
+    "quantity": 20,
+    "desc": "Pulsera de perlas de agua dulce, con cierre de plata 925",
+    "price": 800,
+    "img": "../static/images/productos/pulseras/perlas.png",
+    "category": "pulseras"
+  },
+  {
+    "id": 6,
+    "name": "Pulsera de plata",
+    "quantity": 10,
+    "desc": "Pulsera de plata 925, con dije de perla de agua dulce",
+    "price": 1000,
+    "img": "../static/images/productos/pulseras/plata.png",
+    "category": "pulseras"
+  },
+  {
+    "id": 7,
+    "name": "Pulsera de oro",
+    "quantity": 10,
+    "desc": "Pulsera de oro 18k, con dije de perla de agua dulce",
+    "price": 1500,
+    "img": "../static/images/productos/pulseras/oro.png",
+    "category": "pulseras"
+  },
+  {
+    "id": 8,
+    "name": "Pulsera de diamantes",
+    "quantity": 15,
+    "desc": "Pulsera de oro 18k, con dije de perla de agua dulce y diamantes",
+    "price": 2500,
+    "img": "../static/images/productos/pulseras/diamantes.png",
+    "category": "pulseras"
+  },
+  {
+    "id": 9,
+    "name": "Anillo de perlas",
+    "quantity": 5,
+    "desc": "Anillo de perlas de agua dulce, con cierre de plata 925",
+    "price": 1000,
+    "img": "../static/images/productos/anillos/perlas.png",
+    "category": "anillos"
+  },
+  {
+    "id": 10,
+    "name": "Anillo de plata",
+    "quantity": 6,
+    "desc": "Anillo de plata 925, con dije de perla de agua dulce",
+    "price": 1200,
+    "img": "../static/images/productos/anillos/plata.png",
+    "category": "anillos"
+  },
+  {
+    "id": 11,
+    "name": "Anillo de oro",
+    "quantity": 3,
+    "desc": "Anillo de oro 18k, con dije de perla de agua dulce",
+    "price": 1800,
+    "img": "../static/images/productos/anillos/oro.png",
+    "category": "anillos"
+  },
+  {
+    "id": 12,
+    "name": "Anillo de diamantes",
+    "quantity": 2,
+    "desc": "Anillo de oro 18k, con dije de perla de agua dulce y diamantes",
+    "price": 3000,
+    "img": "../static/images/productos/anillos/diamantes.png",
+    "category": "anillos"
+  }
+]
